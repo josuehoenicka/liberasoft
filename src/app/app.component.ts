03fe38def710f678ba1cece93c33f6bcbdf61401
Josué Hoenicka <josuee.vzla@gmail.com>
2023-05-30 16:21:27 -0300

    add httpclient & rxjs / create getCities method / test cities service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CitiesService } from './service/cities.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private citiesService: CitiesService) { }
+
+  ngOnInit(): void {
+    this.citiesService.getCities().subscribe(
+      data => {
+        console.log(data.data[0]);
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
+
 }
--- src/app/service/cities.service.ts
+++ src/app/service/cities.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CitiesService {
 
-  constructor() { }
+  private apiUrl = 'http://api.dev.liberasoft.com.ar/api/examen';
+
+  constructor(private http: HttpClient) { }
+
+  getCities(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
 }
