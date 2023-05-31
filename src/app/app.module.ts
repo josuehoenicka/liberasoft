efb632e2585b30a563861e12786000f3e546d577
Josué Hoenicka <josuee.vzla@gmail.com>
2023-05-31 08:57:34 -0300

    import table module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+import { TableModule } from 'primeng/table';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
