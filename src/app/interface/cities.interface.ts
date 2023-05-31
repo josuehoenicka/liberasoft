c6c85556a2b782fce34f2b8e3d4461ed4398e3f7
Josué Hoenicka <josuee.vzla@gmail.com>
2023-05-31 08:12:49 -0300

    create cities interface

---
+++ src/app/interface/cities.interface.ts
@@ -0,0 +1,11 @@
+export interface CitiesRespondeI {
+  rta: string;
+  data: CityResponseI[];
+}
+
+export interface CityResponseI {
+  CP: string;
+  ID: number;
+  CIUDADID: string;
+  PROVINCIA: string;
+}
