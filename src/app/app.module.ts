f92177013584907361ddec7ef253856a78df2a9e
Josué Hoenicka <josuee.vzla@gmail.com>
2023-06-02 12:03:40 -0300

    add primeng modules & dialog service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { ToastModule } from 'primeng/toast';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { FileUploadModule } from 'primeng/fileupload';
 import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { DialogModule } from 'primeng/dialog';
+import { DialogService, DynamicDialogModule } from 'primeng/dynamicdialog';
+import { ConfirmationService, MessageService } from 'primeng/api';
+
 
 import { AppComponent } from './app.component';
+
+import { CitiesService } from './service/cities.service';
 
 @NgModule({
   declarations: [
@@ -13,10 +25,19 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
-    TableModule
+    TableModule,
+    ToastModule,
+    ToolbarModule,
+    FileUploadModule,
+    ButtonModule,
+    InputTextModule,
+    DialogModule,
+    DynamicDialogModule
   ],
-  providers: [],
+  providers: [CitiesService, DialogService, ConfirmationService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
