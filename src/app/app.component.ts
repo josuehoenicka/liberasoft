cfbcc2092e8077daafcf8ee7da612caf32423545
Josué Hoenicka <josuee.vzla@gmail.com>
2023-06-02 12:10:26 -0300

    add crud methods / import interfaces / update add & delete method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CitiesService } from './service/cities.service';
-import { CityResponseI } from './interface/cities.interface';
+import { CitiesResponseI, CityResponseI } from './interface/cities.interface';
 
 @Component({
   selector: 'app-root',
@@ -8,20 +8,88 @@ import { CityResponseI } from './interface/cities.interface';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  cities: CitiesResponseI = {
+    rta: '',
+    data: []
+  };
+  selectedCities: CityResponseI[] = [];
+  newCity: CityResponseI = {
+    CP: '',
+    ID: 0,
+    CIUDADID: '',
+    PROVINCIA: ''
+  };
+  citySelected: boolean = false;
 
-  cities: CityResponseI[] = [];
 
-  constructor(private citiesService: CitiesService) { }
+  constructor(private cityService: CitiesService) {}
 
-  ngOnInit(): void {
-    this.citiesService.getCities().subscribe(
-      response => {
-        this.cities = response.data;
+  ngOnInit() {
+    this.fetchCities();
+  }
+
+
+  fetchCities() {
+    this.cityService.getCities().subscribe(
+      (response: CitiesResponseI) => {
+        this.cities = response;
       },
-      error => {
+      (error) => {
         console.error(error);
       }
     );
+  }
+
+  editCity(city: CityResponseI){
+  }
+
+  saveCity() {
+    const newCity = {
+      CP: this.newCity.CP,
+      ID: this.newCity.ID,
+      CIUDADID: this.newCity.CIUDADID,
+      PROVINCIA: this.newCity.PROVINCIA
+    };
+    this.cityService.addCity(newCity).subscribe(
+      () => {
+        this.cities.data.unshift(newCity);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+
+  cancel() {
+    this.newCity = { ...this.newCity, CP: '', ID: 0, CIUDADID: '', PROVINCIA: '' };
+  }
+
+  deleteCity(city: CityResponseI) {
+    this.cityService.deleteCity(city.ID).subscribe(
+      () => {
+        this.cities.data = this.cities.data.filter((c) => c.ID !== city.ID);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  deleteSelectedCities() {
+    if (this.selectedCities.length > 0) {
+      this.selectedCities.forEach(city => {
+        this.cityService.deleteCity(city.ID).subscribe(
+          () => {
+            this.cities.data = this.cities.data.filter(c => c.ID !== city.ID);
+          },
+          error => {
+            console.error(error);
+          }
+        );
+      });
+      this.selectedCities = [];
+    }
   }
 
 }
